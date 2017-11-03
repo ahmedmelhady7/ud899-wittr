f7f5114a6a87bc87b0a9d80cc838f07a26f39955
Ahmed Elhady <ahmed.elhadi@robustastudio.com>
2017-11-03 09:47:30 +0200

    respond to all images with the dr-evil gif

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,14 @@
 self.addEventListener('fetch', function(event) {
   // TODO: only respond to requests with a
   // url ending in ".jpg"
-  event.respondWith(
-    fetch('/imgs/dr-evil.gif')
-  );
+  // if(event.request)
+  let regex = /\.jpg$/;
+  if(event.request.url.match(regex)) {
+  	console.log('success');
+  	event.respondWith(
+  		fetch('/imgs/dr-evil.gif')
+  	);
+  } else {
+  	console.log(event.request.url, event.request.url.match(regex));
+  }
 });
