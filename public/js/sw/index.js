09aea96e467c0df046ccaedd6d5d13ea76250f90
Ahmed Elhady <ahmed.elhadi@robustastudio.com>
2017-11-03 09:18:23 +0200

    hijack requests through sw using a simple response

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,12 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+	var headers = new Headers();
+	headers.append('Content-Type', 'text/html');
+	headers.append('foo', 'bar');
+	event.respondWith(
+		new Response('<h1 class="a-winner-is-me">Hello World!</h1>', {
+			headers
+		})
+	);
 });
