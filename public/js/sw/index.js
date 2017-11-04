62cd445fec50ecc04a4ff1e0c0a2cb4357d18420
Ahmed Elhady <ahmed.elhadi@robustastudio.com>
2017-11-04 11:53:00 +0200

    add scalable way to remove out of date caches using version number

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,9 +1,15 @@
+var CACHE_VERSION = 4;
+
+// Shorthand identifier mapped to specific versioned cache.
+var CURRENT_CACHES = {
+  name: 'wittr-static-v' + CACHE_VERSION
+};
 self.addEventListener('install', function(event) {
   event.waitUntil(
     // TODO: change the site's theme, eg swap the vars in public/scss/_theme.scss
     // Ensure at least $primary-color changes
     // TODO: change cache name to 'wittr-static-v2'
-    caches.open('wittr-static-v2').then(function(cache) {
+    caches.open(CURRENT_CACHES.name).then(function(cache) {
       return cache.addAll([
         '/',
         'js/main.js',
@@ -17,9 +23,22 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('activate', function(event) {
+  const expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
+    return CURRENT_CACHES[key];
+  });
   event.waitUntil(
     // TODO: remove the old cache
-    caches.delete('wittr-static-v1')
+    // caches.delete('wittr-static-v1')
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName.startsWith('wittr-') && expectedCacheNames.indexOf(cacheName) === -1) {
+            console.log('Deleting out of date cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
